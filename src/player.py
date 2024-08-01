747353d7964d9343826e1b2a7a68c226ef06504f
noobyanjunhao <yanaw@bc.edu>
2024-08-01 17:08:10 +0800

    player attributes and actions

--- src/player.py
+++ src/player.py
@@ -11,3 +11,13 @@ class Player:
     
     def send_emoji(self, emoji): #need front end 
         self.emoji = emoji
+
+    def decide_bet_amount(self):
+        #front end part should be responsible for handling or excluding value error
+        amount = int(input(f"{self.name}, enter your bet amount (1 - {self.stack}): "))
+    def place_bet(self, amount):
+        if amount > self.stack:
+            raise ValueError("Bet amount exceeds player's stack")
+        self.stack -= amount
+        return amount
+    
